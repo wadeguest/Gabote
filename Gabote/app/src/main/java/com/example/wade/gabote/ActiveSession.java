f611f430eef218954b46cfc4895849d3f19db0ef
wadeguest <wade.guest@hotmail.ca>
2015-10-29 19:36:04 -0600

    Added a small class to store and retrieve active user id

---
+++ Gabote/app/src/main/java/com/example/wade/gabote/ActiveSession.java
@@ -0,0 +1,10 @@
+package com.example.wade.gabote;
+
+/**
+ * Created by Wade on 2015-10-29.
+ */
+public class ActiveSession {
+    private int activeUserId;
+    public void setActiveUserId(int id){activeUserId=id;}
+    public int getActiveUserId(int id){return activeUserId;}
+}
